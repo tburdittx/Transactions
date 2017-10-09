e6b693fa7861b46bdfd769e3cdc93fdda8806765
Tara <tburdittx@live.com>
2017-10-09 11:43:12 +0100

    Transaction Changes

---
+++ Task 3/Json File/Json File/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using NLog.Config;
+using NLog;
+using NLog.Targets;
+using NLog.Fluent;
+using Newtonsoft.Json;
+
+
+namespace Transactions
+{
+    class Program
+    {
+        public static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+
+            
+
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"C:\Work\Task 3\transactionlog.log", Layout = @"${longdate} ${level}-${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+            
+            var path = @"C:\Work\Task 3\Transactions2013.json";
+            //Read all text into string dataJson
+            var transactions = JsonConvert.DeserializeObject<List<Transaction>>(File.ReadAllText(path));
+
+           // string readText = File.ReadAllText(path);
+
+            logger.Log(LogLevel.Info, "Logger started");
+
+
+            //using (var RDR = new StreamReader(@"C:\Work\Task 3\Transactions2013.json"))
+            //{
+            //   if (!RDR.EndOfStream)  { RDR.ReadLine(); }
+            //   JsonConvert.DeserializeObject<List<Transaction>>(@"C:\Work\Task 3\Transactions2013.json");                
+            //    while (!RDR.EndOfStream)
+            //    {
+            //        try
+            //        {
+            //            var splits = RDR.ReadLine().Split(',');
+            //            var transaction = new Transaction(splits[0], splits[1], splits[2], splits[3], splits[4]);
+
+            //            transactions.Add(transaction);
+            //        }
+            //        catch
+            //        {
+            //            logger.Log(LogLevel.Error, "One of the transactions is dodgy");
+            //        }
+            //    }
+            //}
+
+            Console.WriteLine("Transactions:");
+            foreach (var transaction in transactions)
+            {
+                Console.WriteLine("{0} owes {1} £{2}", transaction.FromAccount, transaction.ToAccount, transaction.Amount);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+    public class Transaction
+    {
+
+        public string Date;
+        public string ToAccount;
+        public string FromAccount;
+        public string Narative;
+        public decimal Amount;
+
+
+        public Transaction(string Date, string To, string From, string Narative, string Amount)
+        {
+            this.Date = Date;
+            this.ToAccount = To;
+            this.FromAccount = From;
+            this.Narative = Narative;
+            this.Amount = decimal.Parse(Amount);
+        }
+        //public string GetDescription()
+        //{
+        //    return Date+ " "+ To+" "+ From+ " "+ Narative+" " + Amount;
+    }
+
+    public class CustomException
+    {
+        private string v;
+        private List<Transaction> transactions;
+
+        public CustomException(string v)
+        {
+            this.v = v;
+        }
+
+        public CustomException(List<Transaction> transactions)
+        {
+            this.transactions = transactions;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Task 3/SupportBank/SupportBank/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+
+
+namespace Transactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Transactions transaction1 = new Transactions();
+                //Transactions transaction2 = new Transactions();
+                //Transactions transaction3 = new Transactions();
+                //Transactions transaction4 = new Transactions();
+                //Transactions transaction5 = new Transactions();
+                //Console.WriteLine(transaction1.GetDescription());
+                //Console.WriteLine(transaction2.GetDescription());
+                //Console.WriteLine(transaction3);
+                // Console.ReadLine();
+
+                // List<string> myList = new List<string>();
+                //List<decimal> mylist = new List<decimal>();
+
+                // Console.WriteLine(myList.Aggregate((a, n) => a + "," + n));
+                // Console.ReadLine();
+
+            }       
+            {
+                List<Transaction> transactions = new List<Transaction>();
+                
+
+                using (var RD = new StreamReader(@"C:\Work\Task 3\Transactions2014.csv"))
+                {
+                    RD.ReadLine();
+                    while (!RD.EndOfStream)
+                    {
+                        var splits = RD.ReadLine().Split(',');
+                        var transaction = new Transaction(splits[0], splits[1], splits[2], splits[3], splits[4]);
+
+                        transactions.Add(transaction);
+
+                    }
+                    {
+                        
+                    }
+                }
+
+                Console.WriteLine("Transactions:");
+                foreach (var transaction in transactions)
+                {
+                    Console.WriteLine("{0} owes {1} £{2}", transaction.From, transaction.To, transaction.Amount);
+                }
+
+                Console.ReadLine();
+            }
+           
+        }
+    }
+    class Transaction
+    {
+        public string Date;
+        public string To;
+        public string From;
+        public string Narative;
+        public decimal Amount;
+
+        public Transaction()
+        {
+            //Date = splits[1] ;
+            //To = ;
+            //From = ;
+            //Narative = ;
+            //Amount = 9.5M;
+        }
+        public Transaction(string Date, string To, string From, string Narative, string Amount)
+        {
+            this.Date = Date;
+            this.To = To;
+            this.From = From;
+            this.Narative = Narative;
+            this.Amount = decimal.Parse(Amount);
+        }
+        //public string GetDescription()
+        //{
+        //    return Date+ " "+ To+" "+ From+ " "+ Narative+" " + Amount;
+        }
+
+       
+    }
+    
+
+
